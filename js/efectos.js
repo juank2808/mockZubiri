4857b6bfb1685898315d37a85691b7d126818465
juankcol <jamayafl15dw@ikzubirimanteo.com>
2016-10-31 09:39:27 +0000

    cosas nuevas

--- js/efectos.js
+++ js/efectos.js
@@ -11,5 +11,21 @@
 
    
     $('.carousel.carousel-slider').carousel({full_width: true});
-     
+    
+ //efecto scroll
+ $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+    }
+  });
+});
+// fin del efecto
  });
